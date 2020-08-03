e8e271ddc034559a6402dede64e1dc221f856921
neural-CODERS <68462534+neural-CODERS@users.noreply.github.com>
2020-08-03 20:28:36 +0530

    Update gaussian_noise.py

--- gaussian_noise.py
+++ gaussian_noise.py
@@ -2,7 +2,7 @@ import cv2
 from skimage.util import random_noise
 import numpy as np 
 
-img = cv2.imread("F:\\Downloads\\histogram2.png",cv2.IMREAD_COLOR)
+img = cv2.imread("Demo_Image.png",cv2.IMREAD_COLOR)
 noise_img = random_noise(img,mode='gaussian',mean=0,var=0.008)
 noise_img = np.array(object=255*noise_img,dtype='uint8')
 cv2.imshow('Noise img',noise_img)
